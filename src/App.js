4dda71dcd913197f369c6b058634405e61f22a71
Seemachal Goud <80570045+Seemachal-Goud-au19@users.noreply.github.com>
2021-12-13 19:10:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"
+import { NestedReal } from "./DynamicList/NestedReal"
+
+const App = () => {
+  return (
+    <div className="App">
+      <NestedReal />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/DynamicList/Data.js
@@ -0,0 +1,13 @@
+export const datas=[
+    {state:"gujarat",
+    cities:[{name:"surat"},{name:"Ahmedabad"},{name:"navsari"}]
+},
+
+    {state:"mharashtra",
+    cities:[{name:"Pune"},{name:"mumbai"},{name:"Nagpur"}]
+     },
+
+    {state:"Andhra",
+    cities:[{name:"Visakhapatnam"},{name:"Vijayawada"},{name:"Guntur"}]
+}
+]
